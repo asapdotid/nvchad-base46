6f8b571b8050d1126a5a05ceb023d1d057485885
Asapdotid <asapdotid@gmail.com>
2023-04-16 18:19:00 +0700

    👌 IMPROVE: update dashboard color

--- lua/base46/integrations/nvdash.lua
+++ lua/base46/integrations/nvdash.lua
@@ -3,11 +3,11 @@ local colors = require("base46").get_theme_tb "base_30"
 return {
   NvDashAscii = {
     fg = colors.black,
-    -- bg = colors.blue,
+    bg = colors.black,
   },
 
   NvDashButtons = {
     fg = colors.white,
-    -- bg = colors.black2,
+    bg = colors.black,
   },
 }
